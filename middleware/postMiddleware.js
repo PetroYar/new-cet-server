c6f3597919bf91c38b6138258f6feaf65e7d634e
PetroYar <petryarish@gmail.com>
2025-02-15 21:50:58 +0200

    0.4

--- middleware/postMiddleware.js
+++ middleware/postMiddleware.js
@@ -8,7 +8,7 @@ export const postMiddleware = (req, res, next) => {
 
   const token = authHeader.split(" ")[1];
 
-  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+  jwt.verify(token, process.env.SECRET, (err, decoded) => {
     if (err) {
       return res
         .status(403)
